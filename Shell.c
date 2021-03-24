7922d9e7230c95323cf6e21fe130dbbeb83e55a4
DavidTK1198 <ddanielb09@gmail.com>
2021-03-24 13:56:56 -0700

    Estructura del proyecto

--- Shell.c
+++ Shell.c
@@ -5,14 +5,17 @@ PROYECTO #1
 ESTUDIANTES: DAVID BARRIENTOS, DANIEL MADRIGAL
 PROFESOR: JOSE CALVO SUÁREZ
 */
-#include <stdio.h>
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 bool shell_Loop();
 int main(){
     pid_t pid;
     bool flag;
+    shell_Loop();
     pid=fork();
     if(pid < 0){
         printf("Error en la llamada fork()");
@@ -25,4 +28,23 @@ int main(){
         printf("hola soy el padre\n");
     }
     return 0;
+}
+
+bool shell_Loop(){
+    bool flag=false;
+     char *str, *fgets_return;
+      int temp;
+  
+    char* command;
+    while (flag==false)
+    {
+        strcpy(str, "");
+        printf("%s\n","@User>>>");
+      fgets_return = fgets( str, sizeof(str), stdin);
+      printf("%s\n",str);
+      if(str=="exit"){
+          flag=true;
+      }
+    }
+    return flag;
 }
