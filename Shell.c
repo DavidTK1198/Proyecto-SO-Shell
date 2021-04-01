84865a964bdf00c27bef7363ca158fe2354a43d7
DavidTK1198 <ddanielb09@gmail.com>
2021-03-31 17:05:03 -0700

    fork

--- Shell.c
+++ Shell.c
@@ -6,6 +6,7 @@ ESTUDIANTES: DAVID BARRIENTOS, DANIEL MADRIGAL
 PROFESOR: JOSE CALVO SUÁREZ
 
 REFERENCIAS:
+https://overiq.com/c-programming-101/array-of-strings-in-c/
 https://www.ibm.com/support/knowledgecenter/SSLTBW_2.2.0/com.ibm.zos.v2r2.bpxbd00/rtgtlgr.htm
 */
 
@@ -16,63 +17,70 @@ https://www.ibm.com/support/knowledgecenter/SSLTBW_2.2.0/com.ibm.zos.v2r2.bpxbd0
 #include <string.h>
 #include <signal.h>
 #include <wait.h>
-int main(){
+void VerificacionDeProcesos(pid_t pid ,char test [15][15],char str [] );
+int main(int argc, char *argv[]){
 
     pid_t pid;
-    int n = 0;
+    int n = argc;
     char* username;
     username=getlogin();
-    bool flag=false;
-    char str[80]="";
+    char str[80] = "";
     int tuberia[2];
     pipe(tuberia);
-    char* test[] = {};
-    pid=fork();
+    char test[15][15]={};
+    pid = fork();
     
-    while (flag==false)
+    if (pid < 0)
     {
-    if(strcmp(str,"exit")==0){
-        flag=true;
-    }
-    if(pid < 0){
         printf("Error en la llamada fork()");
-
         _exit(-1);
-    }else if(pid==0){
-        raise(SIGSTOP);
-        sleep(5);
+    }
+    else if (pid == 0)
+    {
         printf("hola soy el hijo");
-        int contador = 0;
-        close(tuberia[1]);
-        read(tuberia[0],str,sizeof(str));
+        read(tuberia[0], str, sizeof(str));
+   sleep(3);
         n = strlen(str);
+        int contador=0;
         char aux[30] = "";
-        for(int i=0; i<n-1; i++){
-            if(str[i] == ' '){
-                test[contador] = aux;
-                contador++;
+        for (int i = 0; i <= n - 1; i++)
+        {
+            if (str[i] == ' ')
+            {
+          strcpy(test[contador], aux);
+            //strncpy(aux, "\0", sizeof(aux));// ./bin/ps ps 
+            contador++;
             }
             aux[i] = str[i];
         }
-        execlp(test[0],test[0],NULL);
-
-
-    }else{
-        waitpid(pid,NULL,WUNTRACED);
-        close(tuberia[0]);
-        strcpy(str,"");
-        printf("%s%s%s","@",username,">>>");
-        gets(str);
-        printf("%s\n",str);
-        write(tuberia[1],str,sizeof(str));
-        printf("hola soy el padre\n");
-        wait(NULL);
-        kill(pid,SIGCONT);
-        close(tuberia[1]);
+        execlp(test[0], test[0], NULL);
     }
-      
+    else
+    {
+        while (true)
+        {
+
+            if (strcmp(str, "exit") == 0)
+            {
+                break;
+                close(tuberia[1]);
+                close(tuberia[0]);
+            }
+        strcpy(str, "");
+        printf("%s%s%s", "@", username, ">>>");
+        gets(str);
+        write(tuberia[1],str,sizeof(str));
+            wait(NULL);
+            pid = fork();
+            VerificacionDeProcesos(pid,test,str);
+  
+        }
     }
     
     return 0;
     //hola
+}
+
+void VerificacionDeProcesos(pid_t pid,char test [15][15], char str[]){
+    sleep(2);
 }
