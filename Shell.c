6609ac049fcb8b7c6d03165dac4c468f3aa7946f
DavidTK1198 <ddanielb09@gmail.com>
2021-03-31 17:46:59 -0700

    initProcess function

--- Shell.c
+++ Shell.c
@@ -9,7 +9,6 @@ REFERENCIAS:
 https://overiq.com/c-programming-101/array-of-strings-in-c/
 https://www.ibm.com/support/knowledgecenter/SSLTBW_2.2.0/com.ibm.zos.v2r2.bpxbd00/rtgtlgr.htm
 */
-
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdbool.h>
@@ -17,29 +16,54 @@ https://www.ibm.com/support/knowledgecenter/SSLTBW_2.2.0/com.ibm.zos.v2r2.bpxbd0
 #include <string.h>
 #include <signal.h>
 #include <wait.h>
-void VerificacionDeProcesos(pid_t pid ,char test [15][15],char str [] );
-int main(int argc, char *argv[]){
-
+int n=0;
+int tuberia[2];
+ char str[80] = "";
+ char test[15][15]={};
+ void initProcess();
+int main(){
     pid_t pid;
-    int n = argc;
     char* username;
     username=getlogin();
-    char str[80] = "";
-    int tuberia[2];
     pipe(tuberia);
-    char test[15][15]={};
     pid = fork();
-    
     if (pid < 0)
     {
         printf("Error en la llamada fork()");
         _exit(-1);
     }
-    else if (pid == 0)
-    {
+    else if (pid == 0){
         printf("hola soy el hijo");
-        read(tuberia[0], str, sizeof(str));
-   sleep(3);
+        initProcess();
+    }
+    else{
+        while (true){
+        strcpy(str, "");
+        printf("%s%s%s", "@", username, ">>>");
+        gets(str);
+        if (strcmp(str, "exit") == 0){
+                break;
+                close(tuberia[1]);
+                close(tuberia[0]);
+                }
+        write(tuberia[1],str,sizeof(str));
+            wait(NULL);
+            pid = fork();
+        
+            if(pid==0){
+            initProcess();
+            }
+  
+        }
+    }
+    
+    return 0;
+    //hola
+}
+
+void initProcess(){
+    read(tuberia[0], str, sizeof(str));
+        sleep(3);
         n = strlen(str);
         int contador=0;
         char aux[30] = "";
@@ -53,34 +77,5 @@ int main(int argc, char *argv[]){
             }
             aux[i] = str[i];
         }
-        execlp(test[0], test[0], NULL);
-    }
-    else
-    {
-        while (true)
-        {
-
-            if (strcmp(str, "exit") == 0)
-            {
-                break;
-                close(tuberia[1]);
-                close(tuberia[0]);
-            }
-        strcpy(str, "");
-        printf("%s%s%s", "@", username, ">>>");
-        gets(str);
-        write(tuberia[1],str,sizeof(str));
-            wait(NULL);
-            pid = fork();
-            VerificacionDeProcesos(pid,test,str);
-  
-        }
-    }
-    
-    return 0;
-    //hola
-}
-
-void VerificacionDeProcesos(pid_t pid,char test [15][15], char str[]){
-    sleep(2);
+        execlp(test[0], test[0], NULL); 
 }
