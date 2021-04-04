7698164ce6aed6d25d69c9f33948ddd3b4a6c8d8
DavidTK1198 <ddanielb09@gmail.com>
2021-04-04 08:15:34 -0700

    works

--- Shell.c
+++ Shell.c
@@ -23,77 +23,75 @@ https://www.ibm.com/support/knowledgecenter/SSLTBW_2.2.0/com.ibm.zos.v2r2.bpxbd0
 int fd[2];
 char str[80];
 int n = 0;
-bool checkPipe(char**);
-bool getNextLine(char**);
+bool checkPipe(char **);
+bool getNextLine(char **,int*);
 void pipeProcess();
 void pipeProcess2();
-void initProcess();
-void  convertToString (char**);
+void initProcess(char**);
+void convertToString(char **);
 int main()
 {
     pid_t pid;
     pid_t pid2;
-    bool flag=false;
-    char* argv[100];
-    if (pid < 0)
+    bool flag = false;
+    char *argv[100];
+    while (true)
     {
-        printf("Error en la llamada fork()");
-        _exit(-1);
-    }
-    else if (pid == 0)
-    {       read(fd[0], str, sizeof(str));
-                convertToString(argv);
-                flag=checkPipe(argv);
-                initProcess();
-    }
-    else
-    {
-        while (true)
-        {   pipe(fd);
-            pid = fork();
-            if (pid == 0)
-            {   read(fd[0], str, sizeof(str));
-                convertToString(argv);
-                flag=checkPipe(argv);
-                initProcess();
+        pipe(fd);
+        pid = fork();
+        if (pid < 0)
+        {
+            printf("Error en la llamada a fork()");
+            exit(1);
+        }
+        if (pid == 0)
+        {  
+            read(fd[0], str, sizeof(str));
+            n=strlen(str);
+            if (str[n - 1] == '&')
+            {
+                str[n - 1] = (char)13;
             }
             convertToString(argv);
-            flag=getNextLine(argv);
-            if(flag){
-                pid2=fork();
-                 if(pid < 0){
-        printf("Error en la llamada fork()");
-
-        _exit(-1);
-    }else if(pid==0){
-       read(fd[0], str, sizeof(str));
-                convertToString(argv);
-                flag=checkPipe(argv);
-                initProcess();
-    }else{
-       write(fd[1], str, sizeof(str));
-    }
-            }
-            if (strcmp(str, "exit") == 0)
+            flag = checkPipe(argv);
+            initProcess(argv);
+        }
+        else
+        {
+            flag = getNextLine(argv,&pid);
+            if (flag)
             {
-                kill(pid, SIGKILL);
-                break;
+                pid2 = fork();
+                if (pid2 < 0)
+                {
+                    printf("Error en la llamada a fork()");
+                    exit(1);
+                }
+                if (pid2 == 0)
+                {
+                }
+                else
+                {
+                    write(fd[1], str, sizeof(str));
+                }
             }
-            n = strlen(str);
             close(fd[1]);
             close(fd[0]);
             if (str[n - 1] == '&')
             {
                 waitpid(pid, NULL, WUNTRACED);
-                  int c = getchar();
+                int c = getchar();
             }
             else
             {
-                if(flag){
-                    waitpid(pid,NULL,0);
-                waitpid(pid2,NULL,0);
-                }else{
-                wait(NULL);
+                if (flag)
+                {
+                    waitpid(pid, NULL, 0);
+                    waitpid(pid2, NULL, 0);
+                }
+                else
+                {
+                    wait(NULL);
                 }
             }
         }
@@ -101,18 +99,21 @@ int main()
     return 0;
 }
 
-void pipeProcess(){
-        dup2(fd[1],STDOUT_FILENO);
-                close(fd[0]);
-                close(fd[1]);
+void pipeProcess()
+{
+    dup2(fd[1], STDOUT_FILENO);
+    close(fd[0]);
+    close(fd[1]);
 }
-void pipeProcess2(){
-    dup2(fd[0],STDIN_FILENO);
-        close(fd[0]);
-        close(fd[1]);
+void pipeProcess2()
+{
+    dup2(fd[0], STDIN_FILENO);
+    close(fd[0]);
+    close(fd[1]);
 }
 
-void  convertToString (char** worker){
+void convertToString(char **worker)
+{
     char *token;
     token = strtok(str, " ");
     int i = 0;
@@ -124,43 +125,44 @@ void  convertToString (char** worker){
     }
     worker[i] = NULL;
 }
-bool checkPipe(char** argv){
-    char* helpme;
-for (int i = 0; i < n; i++)
-        { 
-            helpme = strchr(argv[i], '|');
-            if (helpme != NULL)
-            {
-                return true;
-            
-            }
+bool checkPipe(char **argv)
+{
+    char *helpme;
+    n = strlen(str);
+    for (int i = 0; i < n - 1; i++)
+    {
+        helpme = strchr(argv[i], '|');
+        if (helpme != NULL)
+        {
+            return true;
         }
     }
-bool getNextLine(char** ar){
-        char *username;
-         username = getlogin();
-            strcpy(str, "");
-            printf("%s%s%s", "@", username, ">>>$");
-            gets(str);
-            convertToString(ar);
-            if(checkPipe(ar)){
-            write(fd[1], str, sizeof(str));
-            return true;
-            }
-            write(fd[1], str, sizeof(str));
-            return false;
+    return false;
 }
-void initProcess()
-{ //hace faltar hacer los if & y |
-    n = strlen(str);
-    char *argv[100];
-    int j = 0;
-    char aux[80] = "";
-    if (str[n - 1] == '&')
-    {
-        str[n - 1] = (char)13;
+bool getNextLine(char **ar,int* pid)
+{
+    char *username;
+    char aux[80];
+    username = getlogin();
+    strcpy(str, "");
+    printf("%s%s%s", "@", username, ">>>$");
+    gets(str);
+    strcpy(aux, str);
+    if (strcmp(str, "exit") == 0)
+    {   kill(*pid, SIGKILL);
+        exit(1);
     }
-    convertToString(argv);
+    convertToString(ar);
+    if (checkPipe(ar))
+    {
+        write(fd[1], aux, sizeof(str));
+        return true;
+    }
+    write(fd[1], aux, sizeof(str));
+    return false;
+}
+void initProcess(char **argv)
+{ //hace faltar hacer los if & y |
     int p = execvp(argv[0], argv);
     printf("%s\n", "Comando o ruta no encontrada");
     exit(-1);
